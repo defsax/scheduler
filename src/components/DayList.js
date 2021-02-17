8400ab2baf4da2d8ce78715ee6cd6f293059ac67
Per <defayetteperry@gmail.com>
2021-02-17 01:24:11 -0500

    Finish interviewerlistitem.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,11 @@ import DayListItem from "components/DayListItem"
 
 export default function DayList(props) {
 
+  console.log(props.days);
+
   const parsedDays = props.days.map(day => (
     <DayListItem 
+      key={day.id}
       name={day.name}
       spots={day.spots}
       selected={day.name === props.day}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,12 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  console.log(props);
-
   const dayClass = classNames(
     "day-list__item",
     {"day-list__item--selected": props.selected}, 
     {"day-list__item--full": props.spots === 0});
 
   const formatSpots = function() {
-    console.log("props spots:",props.spots);
     if (props.spots < 1)
       return "no spots remaining";
     if (props.spots < 2) 
