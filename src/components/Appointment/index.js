e48894603c7bfd64e558e5aacbd460bd7487ec1e
Per <defayetteperry@gmail.com>
2021-02-19 17:51:49 -0500

    Add confirm functionality.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Create from "components/Appointment/Form";
 import Status from "components/Appointment/Status";
+import Confirm from "components/Appointment/Confirm";
 
 import { useVisualMode } from "hooks/useVisualMode"; 
 
@@ -15,6 +16,7 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 
 export default function Appointment(props) {
@@ -50,7 +52,7 @@ export default function Appointment(props) {
         <Show student=
           {props.interview.student} 
           interviewer={props.interview.interviewer}
-          onDelete={cancelInterview}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
 
@@ -62,9 +64,16 @@ export default function Appointment(props) {
         />
       }
       
+      {mode === CONFIRM && (
+        <Confirm 
+          message="Are you sure you want to delete this?" 
+          onConfirm={cancelInterview} 
+          onCancel={() => transition(SHOW)}
+        />
+      )}
+
       {mode === SAVING && (<Status message="Saving..."/>)}
       {mode === DELETING && (<Status message="Deleting..."/>)}
-
     </article>
   );
 }
